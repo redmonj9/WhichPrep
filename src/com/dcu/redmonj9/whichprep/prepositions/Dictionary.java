c3c54a8028dff76b0527d1203b7d32cc5d9374fd
James Redmond <etriel123@gmail.com>
2015-01-21 11:49:37 +0000

    Adding new prepositions + some Javadocs

--- src/com/dcu/redmonj9/whichprep/prepositions/Dictionary.java
+++ src/com/dcu/redmonj9/whichprep/prepositions/Dictionary.java
@@ -6,38 +6,6 @@ public class Dictionary {
 	private static ArrayList<String> dictionary;
 	private static ArrayList<String> prepositions;
 	
-	public Dictionary(){
-		dictionary = new ArrayList<String>();
-		prepositions = new ArrayList<String>();
-		dictionary.add("He will see them in October");
-		dictionary.add("They will meet him in two months");
-		dictionary.add("She put on some music");
-		dictionary.add("He will see them on the 3rd of October");
-		dictionary.add("He will arrive at the airport");
-		dictionary.add("He met her at noon for tea");
-		dictionary.add("They will meet regurlarly over 2 months");
-		dictionary.add("He was pulled over for speeding");
-		dictionary.add("He looked above him for the timetable");
-		dictionary.add("A captain is above a lieutenant");
-		dictionary.add("He considered such an action below his notice");
-		dictionary.add("The water was well below freezing");
-		
-		prepositions.add("in");
-		prepositions.add("on");
-		prepositions.add("at");
-		prepositions.add("over");
-		prepositions.add("above");
-		prepositions.add("below");
-	}
-	
-	public String getDictionaryItem(int index){
-		return dictionary.get(index);
-	}
-	
-	public String getPrepItem(int index) {
-		return prepositions.get(index);
-	}
-	
 	public static ArrayList<String> getDictionary(){
 		dictionary = new ArrayList<String>();
 		dictionary.add("He will see them in October");
@@ -63,6 +31,27 @@ public class Dictionary {
 		prepositions.add("over");
 		prepositions.add("above");
 		prepositions.add("below");
+		prepositions.add("of");
+		prepositions.add("to");
+		prepositions.add("for");
+		prepositions.add("with");
+		prepositions.add("from");
+		prepositions.add("by");
+		prepositions.add("about");
+		prepositions.add("as");
+		prepositions.add("into");
+		prepositions.add("like");
+		prepositions.add("through");
+		prepositions.add("after");
+		prepositions.add("between");
+		prepositions.add("out");
+		prepositions.add("against");
+		prepositions.add("during");
+		prepositions.add("without");
+		prepositions.add("before");
+		prepositions.add("under");
+		prepositions.add("around");
+		prepositions.add("among");
 		return prepositions;
 	}
 }
--- src/com/dcu/redmonj9/whichprep/prepositions/PrepScrubber.java
+++ src/com/dcu/redmonj9/whichprep/prepositions/PrepScrubber.java
@@ -3,8 +3,14 @@ package com.dcu.redmonj9.whichprep.prepositions;
 import java.util.ArrayList;
 
 public class PrepScrubber {
-		
-	public static String containsPrep(String sentence, ArrayList<String> preps){
+	
+  /**
+   * This method scans a sentence for a preposition found in the provided list
+   * @param sentence The sentence to be searched for a preposition
+   * @param preps The list of possible prepositions to search for
+   * @return The preposition found in the sentence
+   */
+  public static String containsPrep(String sentence, ArrayList<String> preps){
 		String result = "";
 		for(String prep: preps){
 			if(sentence.contains(" "+prep+" "))
@@ -13,6 +19,13 @@ public class PrepScrubber {
 		return result;
 	}
 	
+  /**
+   * This method scans the sentence for the given preposition 
+   * and replaces with a string of underscores
+   * @param sentence The sentence to be searched
+   * @param prep The preposition to be removed
+   * @return The sentence with preposition replaced
+   */
 	public static String removePrep(String sentence, String prep){
 		return sentence.replace(prep, "____");
 	}
