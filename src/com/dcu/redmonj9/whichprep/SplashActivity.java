868d6a42f4d248f45898c5a456d454d16cf11ccc
James Redmond <etriel123@gmail.com>
2015-02-03 20:29:51 +0000

    Added a splash screen

---
+++ src/com/dcu/redmonj9/whichprep/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.dcu.redmonj9.whichprep;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+	private static final int TIME = 3000;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Intent i = new Intent(SplashActivity.this, FrontMenuActivity.class);
+				startActivity(i);
+				finish();
+			}
+		}, TIME);
+	}
+}
